a3b671a802a755d9ba8a52491d9bfa11bb7dcccc
rlc900 <rianna.cleary@gmail.com>
2020-10-29 17:32:10 -0400

    tried to fix jest error

---
+++ _mocks_/@fullstackjs/mail.js
@@ -0,0 +1,17 @@
+export default class Mail {
+    to() {
+        return this
+    }
+
+    subject() {
+        return this
+    }
+
+    data() {
+        return this
+    }
+
+    send() {
+        return this
+    }
+}
--- _tests_/server/models/user.spec.js
+++ _tests_/server/models/user.spec.js
@@ -5,8 +5,8 @@ import mongoose from 'mongoose';
 import Bcrypt from 'bcryptjs'
 
 describe('The User model', () => {
-  it('should hash the user password before saving to the database', async() => {
-    await mongoose.connect('mongodb://localhost:27017/auth-app_test' ,{useNewUrlParser: true})
+  it('should hash the user password before saving to the database', async () => {
+    await mongoose.connect('mongodb://localhost:27017/auth-app_test' ,{ useNewUrlParser: true, useUnifiedTopology: true })
 
     const user = {
         name: 'Test User',
@@ -16,5 +16,9 @@ describe('The User model', () => {
 
     const createdUser = await User.create(user) 
     expect(Bcrypt.compareSync(user.password, createdUser.password)).toBe(true)
+
+    await mongoose.connection.close()
+
+    
   })
 })
