60e046265ff0e2e37902b98bf03eb6decc66b7d7
rlc900 <rianna.cleary@gmail.com>
2020-11-03 12:31:57 -0500

    setup and tear down

--- _tests_/server/models/user.spec.js
+++ _tests_/server/models/user.spec.js
@@ -5,8 +5,12 @@ import mongoose from 'mongoose';
 import Bcrypt from 'bcryptjs'
 
 describe('The User model', () => {
-  it('should hash the user password before saving to the database', async () => {
+
+  beforeAll(async () => {
     await mongoose.connect('mongodb://localhost:27017/auth-app_test' ,{ useNewUrlParser: true, useUnifiedTopology: true })
+  })
+
+  it('should hash the user password before saving to the database', async () => {
 
     const user = {
         name: 'Test User',
@@ -16,9 +20,22 @@ describe('The User model', () => {
 
     const createdUser = await User.create(user) 
     expect(Bcrypt.compareSync(user.password, createdUser.password)).toBe(true)
+  })
 
-    await mongoose.connection.close()
+  it('should set the email confirm code for the user before saving to the database', async() => {
+    const user = {
+      name: 'Test User',
+      email: 'test@user.com',
+      password: 'password'
+  }
 
-    
+
+  const createdUser = await User.create(user) 
+  expect(createdUser.emailConfirmCode).toEqual(expect.any(String))
+
+  })
+
+  afterAll(async () => {
+     await mongoose.connection.close()
   })
 })
