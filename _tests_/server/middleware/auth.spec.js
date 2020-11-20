2ee28c024282bc963998ebcff7bc0f805ae373c8
rlc900 <rianna.cleary@gmail.com>
2020-11-20 14:30:33 -0500

    tested middleware

---
+++ _tests_/server/middleware/auth.spec.js
@@ -0,0 +1,82 @@
+/**
+ 
+ * @jest-environment node
+
+ */
+
+import User from '@models/User';
+import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken'
+import config from '@config'
+import authMiddleware from '@middleware/auth'
+
+class Response {
+    status(status) {
+      this.status = status
+ 
+      return this
+    }
+ 
+    json(data) {
+      return data
+    }
+  }
+
+describe('The auth middleware', () => {
+
+  const user = {
+    name: 'Test User',
+    email: 'test@user.com',
+    password: 'password'
+}
+
+  let createdUser
+
+  beforeAll(async () => {
+    await mongoose.connect('mongodb://localhost:27017/auth-app_test' , { useNewUrlParser: true, useUnifiedTopology: true })
+    createdUser = await User.create(user) 
+  })
+
+    it('should call the next function if authentication is successful', async () => {
+      const access_token = createdUser.generateToken()
+      const req = {
+          body: {
+              access_token
+          }
+      }
+
+      const res = new Response()
+      const next = jest.fn()
+
+      await authMiddleware(req, res, next)
+
+      expect(next).toHaveBeenCalled()
+    })
+
+    it('should return a 401 if authentication fails', async () => {
+      const req = {
+          body: {}
+      }
+
+      const res = new Response()
+
+      const statusSpy = jest.spyOn(res, 'status')
+      const jsonSpy = jest.spyOn(res, 'json')
+      const next = jest.fn()
+
+      await authMiddleware(req, res, next)
+
+      expect(next).toHaveBeenCalledTimes(0)
+      expect(statusSpy).toHaveBeenCalledWith(401)
+      expect(jsonSpy).toHaveBeenCalledWith({
+          message: 'Unauthenticated.'
+      })
+
+
+
+    })
+
+  afterAll(async () => {
+     await mongoose.connection.close()
+  })
+})
